93c47d990cd545a246682dcb98419ee99b1644ca
Ba9A-tata <yogadzakkii@gmail.com>
2024-06-03 14:29:42 +0700

    menambahkan if stream in file untuk menunjuk ke sebuah file

--- iofile.cpp
+++ iofile.cpp
@@ -11,4 +11,22 @@ int main() {
     // menunjuk ke sebuah nama file
     outfile.open("contohfile.txt");
 
-   
+     cout << ">= Menulis file, \'q' untuk keluar" << endl;
+
+     //unlimited loop untuk menulis
+     while (true) {
+        cout << "- ";
+        // menambahkan setip karakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        // menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+     }
+     // selesai dalam menulis dan sekarang tutup filenya
+     outfile.close();
+
+     //Membuka file dalam mode membaca
+     ifstream infile;
+     
+}
